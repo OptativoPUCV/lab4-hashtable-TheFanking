b7f435238d79666994945c916478f593543b3c4c
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-04-27 16:57:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -171,9 +171,6 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) 
 {
-  if (map == NULL) {
-    return NULL;
-  }
 
   // Recorre el arreglo de cubetas hasta encontrar el primer elemento no nulo
   for (int i = 0; i < map->capacity; i++) {
