fc3d75620f2fd785e754bd7e847c5c4a04ed557f
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-04-27 00:08:04 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -81,6 +81,7 @@ void insertMap(HashMap * map, char * key, void * value)
            }
          }
      }
+    map->size++;
   }
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
