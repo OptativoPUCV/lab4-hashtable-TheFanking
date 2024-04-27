a6b3e918bc23592b5336d96bcb384f6cb9206bcf
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-04-27 00:42:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -42,6 +42,10 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value) 
 {
   long posicion = hash(key,map->capacity);
+  if (posicion == 0)
+  {
+    exit(EXIT_FAILURE);
+  }
   Pair *mapAux = createPair(key,value);
   if (mapAux == NULL)
     {
