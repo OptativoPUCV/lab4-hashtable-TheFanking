19e2f7b492393b0d4e87a25a20dca905f6a35b7e
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-04-27 18:01:50 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -162,6 +162,14 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) 
 {
+  for (long i = 0 ; i < map->capacity ; i++)
+    {
+      if (map->buckets[i] != NULL && map->buckets[i]->key != NULL)
+      {
+        map->current = i;
+        return map->buckets[i];
+      }
+    }
   return NULL;
   // Si no se encontró ningún elemento no nulo, devuelve NULL
   
