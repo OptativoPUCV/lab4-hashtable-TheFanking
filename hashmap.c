5170578540589a1bd8c1242ae9eedb8b2c3b300f
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-04-27 19:05:29 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -180,21 +180,18 @@ Pair * firstMap(HashMap * map)
 
 Pair * nextMap(HashMap * map) 
 {
-  if (map == NULL || firstMap(map) == NULL)
-  {
+  if (map == NULL || map->current == -1) {
     return NULL;
   }
-  Pair *aux = firstMap(map);
-  long posicion = hash(aux->key,map->capacity);
 
-  for (long i = posicion + 1 ; i < map->capacity ; i++)
-    {
-      if (map->buckets[i] != NULL && map->buckets[i]->key != NULL)
-      {
-        map->current = i;
+  for (long i = map->current + 1; i < map->capacity; i++) {
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+        map->current = i; // Actualiza map->current
         return map->buckets[i];
-      }
     }
+  }
+
+  return NULL;
 
     return NULL;
 }
