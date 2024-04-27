416545314e254bdabee93d51e27540ec0b101b9e
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-04-27 18:31:58 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -184,7 +184,15 @@ Pair * nextMap(HashMap * map)
   {
     return NULL;
   }
-  
+
+  for (long i = map->current + 1 ; i < map->capacity ; i++)
+    {
+      if (map->buckets[i] != NULL && map->buckets[i]->key != NULL)
+      {
+        map->current = i;
+        return map->buckets[i];
+      }
+    }
 
     return NULL;
 }
