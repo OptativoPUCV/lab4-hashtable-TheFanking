592d7decc5ff9f689d3a1de27720d07d1d8b7982
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-04-27 18:34:11 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -189,7 +189,7 @@ Pair * nextMap(HashMap * map)
     {
       if (map->buckets[i] != NULL && map->buckets[i]->key != NULL)
       {
-        map->current = i +1;
+        map->current++;
         return map->buckets[i];
       }
     }
